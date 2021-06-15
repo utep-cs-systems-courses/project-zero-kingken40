b2f25eb3e0dfda61b4a65fd97ccd2c7284a62c5e
kingken40 <45611070+kingken40@users.noreply.github.com>
2021-06-15 14:33:05 -0600

    Update draw.h

--- draw.h
+++ draw.h
@@ -2,6 +2,7 @@
 #define DRAW_INCLUDED
 
 void print_triangle(int startCol, int size);
+void print_arrow(int startCol, int size);
 void print_square(int startCol, int size);
 void print_char_11x16(char c);
 
